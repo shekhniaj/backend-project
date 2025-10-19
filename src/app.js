2b628014baf1d0b7b5cd5fc60015ce610a0b033a
Shekh Niaj <emofix235@gmail.com>
2025-10-18 18:54:28 -0700

    add utils code

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(cookieParser())
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import dotenv from 'dotenv/config'
+import dotenv from "dotenv/config";
 import connectDB from "./db/dbConnection.js";
+import { app } from "./app.js";
+
+const port = process.env.PORT || 4000;
 
 connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("err: ", err);
+      process.exit(1);
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is listening at port ${port}`);
+    });
+  })
+  .catch((err) => console.log("MongoDB connection failed !! ", err));
